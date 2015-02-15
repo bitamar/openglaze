934e9c626e03278fe09d1f6c41c53e45c91bc60f
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2015-02-15 22:05:08 +0200

    Updating glaze and material

---
+++ openglaze/modules/custom/openglaze_restful/plugins/restful/node/companies/1.0/OpenglazeCompaniesResource.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Contains OpenglazeCompaniesResource.
+ */
+class OpenglazeCompaniesResource extends \OpenglazeEntityBaseNode {
+  /**
+   * Overrides \RestfulEntityBaseNode::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    return $public_fields;
+  }
+}
