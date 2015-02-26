ff0916f134447a266d43d35afbb87a8e912b6e21
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2015-02-26 19:51:33 +0200

    Glazes page

--- openglaze/modules/custom/openglaze_restful/plugins/restful/node/glazes/1.0/OpenglazeGlazesResource.class.php
+++ openglaze/modules/custom/openglaze_restful/plugins/restful/node/glazes/1.0/OpenglazeGlazesResource.class.php
@@ -11,6 +11,35 @@ class OpenglazeGlazesResource extends \OpenglazeEntityBaseNode {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
+    $public_fields['ingredients'] = array(
+      'property' => 'field_ingredients',
+      'process_callbacks' => array(
+        array($this, 'processIngredient'),
+      ),
+    );
+
     return $public_fields;
+  }
+
+  /**
+   * Process callback; Clean the ingredient nodes.
+   */
+  protected function processIngredient($ingredient_nodes) {
+    $ingredients = array();
+    foreach ($ingredient_nodes as $node) {
+      $wrapper = entity_metadata_wrapper('node', $node);
+      $material_wrapper = $wrapper->field_material;
+
+      $ingredients[] = array(
+        'material' => array(
+          'id' => $material_wrapper->getIdentifier(),
+          'label' => $material_wrapper->label(),
+          'aka' => $material_wrapper->field_aka->value()[0],
+        ),
+        'amount' => $wrapper->field_amount->value(),
+      );
+    }
+
+    return $ingredients;
   }
 }
